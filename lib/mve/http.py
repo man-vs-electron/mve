ba0ed9cc90e4d92952379ca8d3435c2bed22d30b
Man Vs. Electron <man.vs.electron@gmail.com>
2017-06-01 11:47:36 -0600

    Slight change to http

--- lib/mve/http.py
+++ lib/mve/http.py
@@ -5,14 +5,15 @@ from urlparse import parse_qs, urlparse
 from mve.utils import eprint
 
 class Server(BaseHTTPServer.HTTPServer):    
-    def __init__(self, port):
+    def __init__(self, port, load_modules=True):
         BaseHTTPServer.HTTPServer.__init__(self, ('', port), MyHandler)
         self.modules = {}
-        eprint("Loading modules")
-        for file in glob.glob("module_*.py"):
-            eprint(file)
-            self.modules[file[7:-3]]=__import__(file[:-3].replace("/", "."))
-        eprint("Modules loaded: "+str(self.modules.keys())+"\n\n")
+        if load_modules:
+            eprint("Loading modules")
+            for file in glob.glob("module_*.py"):
+                eprint(file)
+                self.modules[file[7:-3]]=__import__(file[:-3].replace("/", ".")).execute
+            eprint("Modules loaded: "+str(self.modules.keys())+"\n\n")
 
     def finish_request(self, request, client_address):
         request.settimeout(10)
@@ -41,8 +42,8 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             parts = s.path.split('?')[0].split("/")[1:]
             query_dict = parse_qs(urlparse(s.path).query)
             if len(parts)>0 and parts[0] in s.server.modules:
-                module = s.server.modules[parts[0]]
-                module.execute(s, parts, query_dict)
+                execute = s.server.modules[parts[0]]
+                execute(s, parts, query_dict)
             else:
                 s.send_response(404)
                 s.end_headers()
